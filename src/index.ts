1c28e7ab486fe46d7d4825dca6cf1816e6161484
LuizFelipems <luizfelipems12@gmail.com>
2020-11-02 20:48:08 -0300

    principio de Liskov

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
+import AbstractSharedButton from "./AbstractShareButton"
 import ShareButtonTwitter from "./ShareButtonTwitter";
 import ShareButtonFacebook from "./ShareButtonFacebook";
 import ShareButtonLinkedin from "./ShareButtonLinkedin";
 import ShareButtonPrint from "./ShareButtonPrint";
 
-const twitter = new ShareButtonTwitter(".btn-twitter", "https://wwww.youtube.com/rodrigobranas");
+const twitter: AbstractSharedButton = new ShareButtonTwitter(".btn-twitter", "https://wwww.youtube.com/rodrigobranas");
 twitter.bind();
-const facebook = new ShareButtonFacebook(".btn-facebook", "https://wwww.youtube.com/rodrigobranas");
+const facebook: AbstractSharedButton = new ShareButtonFacebook(".btn-facebook", "https://wwww.youtube.com/rodrigobranas");
 facebook.bind();
-const linkedin = new ShareButtonLinkedin(".btn-linkedin", "https://wwww.youtube.com/rodrigobranas");
+const linkedin: AbstractSharedButton = new ShareButtonLinkedin(".btn-linkedin", "https://wwww.youtube.com/rodrigobranas");
 linkedin.bind();
-const print = new ShareButtonPrint(".btn-print");
+const print: AbstractSharedButton = new ShareButtonPrint(".btn-print");
 print.bind();
