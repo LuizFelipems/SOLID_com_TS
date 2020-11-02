ed9a3c03d07efd5829ec10f1602a51de2cc35edc
LuizFelipems <luizfelipems12@gmail.com>
2020-11-02 19:59:21 -0300

    open closed 2 - padrao strategy

---
+++ src/ShareButtonLinkedin.ts
@@ -0,0 +1,11 @@
+import AbstractShareButton from "./AbstractShareButton";
+
+export default class ShareButtonLinkedin extends AbstractShareButton {
+    constructor(clazz: string, url: string) {
+        super(clazz, url);
+    }
+    
+    createLink(): string {
+        return `https://www.linkedin.com/shareArticle?url=${this.url}`;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import ShareButton from "./ShareButton";
+import ShareButtonTwitter from "./ShareButtonTwitter";
+import ShareButtonFacebook from "./ShareButtonFacebook";
+import ShareButtonLinkedin from "./ShareButtonLinkedin";
 
-const shareButton = new ShareButton("https://www.youtube.com/rodrigobranas");
-shareButton.bind(".btn-twitter", "twitter");
-shareButton.bind(".btn-facebook", "facebook");
-shareButton.bind(".btn-linkedin", "linkedin");
+const twitter = new ShareButtonTwitter(".btn-twitter", "https://.youtube.com/rodrigobranas");
+twitter.bind();
+const facebook = new ShareButtonFacebook(".btn-facebook", "https://.youtube.com/rodrigobranas");
+facebook.bind();
+const linkedin = new ShareButtonLinkedin(".btn-linkedin", "https://.youtube.com/rodrigobranas");
+linkedin.bind();
