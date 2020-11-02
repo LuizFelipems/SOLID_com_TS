46c4c43a5f92c24163f3ce6cf7cd15ae85ab44c4
LuizFelipems <luizfelipems12@gmail.com>
2020-11-02 20:08:10 -0300

    correcao url

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import ShareButtonTwitter from "./ShareButtonTwitter";
 import ShareButtonFacebook from "./ShareButtonFacebook";
 import ShareButtonLinkedin from "./ShareButtonLinkedin";
 
-const twitter = new ShareButtonTwitter(".btn-twitter", "https://.youtube.com/rodrigobranas");
+const twitter = new ShareButtonTwitter(".btn-twitter", "https://wwww.youtube.com/rodrigobranas");
 twitter.bind();
-const facebook = new ShareButtonFacebook(".btn-facebook", "https://.youtube.com/rodrigobranas");
+const facebook = new ShareButtonFacebook(".btn-facebook", "https://wwww.youtube.com/rodrigobranas");
 facebook.bind();
-const linkedin = new ShareButtonLinkedin(".btn-linkedin", "https://.youtube.com/rodrigobranas");
+const linkedin = new ShareButtonLinkedin(".btn-linkedin", "https://wwww.youtube.com/rodrigobranas");
 linkedin.bind();
